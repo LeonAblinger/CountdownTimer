17e9280184ac16892364d73f87fc4615d9dc74a6
LeonAblinger <leon.ablinger@outlook.at>
2022-03-10 15:13:29 +0100

    Added application

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CountdownTimer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labCountdown = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // labCountdown
+            // 
+            this.labCountdown.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labCountdown.Location = new System.Drawing.Point(11, 8);
+            this.labCountdown.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labCountdown.Name = "labCountdown";
+            this.labCountdown.Size = new System.Drawing.Size(338, 41);
+            this.labCountdown.TabIndex = 0;
+            this.labCountdown.Text = "label1";
+            this.labCountdown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(19, 67);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(89, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Notenschluss";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(114, 67);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(113, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Schriftliche Matura";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(233, 67);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(110, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Mündliche Matura";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 96);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.labCountdown);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(380, 135);
+            this.MinimumSize = new System.Drawing.Size(380, 135);
+            this.Name = "FormMain";
+            this.Text = "Countdown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labCountdown;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CountdownTimer
+{
+    public partial class FormMain : Form
+    {
+        Control control;
+        DateTime[] dT_Goal = { new DateTime(2022, 4, 8), new DateTime(2022, 5, 5), new DateTime(2022, 6, 23) };
+        int SelectedGoalInd = 0;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            control = labCountdown;
+        }
+
+        private void MainApp()
+        {
+            try
+            {
+                while (true)
+                {
+                    UpdateCountdown(SelectedGoalInd);
+                    Wait(50);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void UpdateCountdown(int _selectedCd)
+        {
+            DateTime dT_Now = DateTime.Now;
+            TimeSpan tS_Diff = dT_Goal[_selectedCd] - dT_Now;
+            String labText = "";
+
+            labText += tS_Diff.Days > 0 ? tS_Diff.Days.ToString() + " Tage  " : "";
+            labText += tS_Diff.Hours < 10 ? "0" + tS_Diff.Hours.ToString() + ":" : tS_Diff.Hours.ToString() + ":";
+            labText += tS_Diff.Minutes < 10 ? "0" + tS_Diff.Minutes.ToString() + ":" : tS_Diff.Minutes.ToString() + ":";
+            labText += tS_Diff.Seconds < 10 ? "0" + tS_Diff.Seconds.ToString() + ":" : tS_Diff.Seconds.ToString() + ".";
+            labText += tS_Diff.Milliseconds < 10 ? "00" + tS_Diff.Milliseconds.ToString() : tS_Diff.Milliseconds < 100 ? "0" + tS_Diff.Milliseconds.ToString() : tS_Diff.Milliseconds.ToString();
+
+            control.BeginInvoke((MethodInvoker)delegate ()
+            {
+                labCountdown.Text = labText;
+            });
+        }
+
+        public void Wait(int ms)
+        {
+            try
+            {
+                var tim_Timer = new System.Windows.Forms.Timer();
+                if (ms == 0 || ms < 0) return;
+
+                tim_Timer.Interval = ms;
+                tim_Timer.Enabled = true;
+                tim_Timer.Start();
+
+                tim_Timer.Tick += (s, e) =>
+                {
+                    tim_Timer.Enabled = false;
+                    tim_Timer.Stop();
+                };
+
+                while (tim_Timer.Enabled)
+                {
+                    Application.DoEvents();
+                    Thread.Sleep(1);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread update = new Thread(MainApp);
+            update.Start();
+        }
+
+        private void SetRadioButton(int _checkedRadioButton)
+        {
+            radioButton1.Checked = _checkedRadioButton == 1 ? true : false;
+            radioButton2.Checked = _checkedRadioButton == 2 ? true : false;
+            radioButton3.Checked = _checkedRadioButton == 3 ? true : false;
+
+            SelectedGoalInd = _checkedRadioButton - 1;
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            SetRadioButton(1);
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            SetRadioButton(2);
+        }
+
+        private void radioButton3_Click(object sender, EventArgs e)
+        {
+            SetRadioButton(3);
+        }
+    }
+}
